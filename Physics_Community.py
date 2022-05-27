19fb92a5a250fe233e49afc81ec5d90447b82cb2
Croitoru-Alexandru <91969171+Croitoru-Alexandru@users.noreply.github.com>
2022-05-28 00:13:53 +0300

    75% terminat

--- Physics_Community.py
+++ Physics_Community.py
@@ -47,20 +47,55 @@ def calcul_ohm():
         rezistenta = float(request.form["rezistenta_ohm"])
         rezistenta_int = float(request.form["rez_int_ohm"])
         intensitatea_ohm = sursa1 / (rezistenta + rezistenta_int)
-        return render_template("calcul_ohm.html", inten_ohm=intensitatea_ohm)
+        raspuns = f'{intensitatea_ohm} A'
+        return render_template("calcul_ohm.html", inten_ohm=raspuns)
     else:
         return render_template("calcul_ohm.html")
 
 
-@app.route('/calcul_intensitate', methods=["POST, GET"])
+@app.route('/calcul_intensitate', methods=["POST", "GET"])
 def calcul_int():
     if request.method == "POST":
         sursa = float(request.form["sursa"])
-        rezistenta_interna = float(request.form["rezistenta_interna"])
-        intensitatea = f"{round(sursa / rezistenta_interna, 2)} A"
-        return render_template("calcul_intensitate.html", inten=intensitatea)
+        rezis_intern = float(request.form["rezistenta_interna"])
+        intens_max = round(sursa / rezis_intern, 2)
+        raspuns = f'{intens_max} A'
+        return render_template("calcul_intensitate.html", intens_max=raspuns)
     else:
         return render_template("calcul_intensitate.html")
+
+
+@app.route('/rezistenta_serie', methods=["POST", "GET"])
+def calculare_rezistenta_serie():
+    if request.method == "POST":
+        rezistente = request.form["input_rezistenta_serie"]
+        rezistenta = rezistente.split(",")
+        r = 0
+        for i in rezistenta:
+            rez = float(i)
+            r += rez
+        x = str(r)
+        return render_template("/rezistenta_serie.html", r=x)
+    else:
+        return render_template("/rezistenta_serie.html")
+
+
+@app.route('/rezistenta_paralel', methods=["POST", "GET"])
+def calculare_rezistenta_paralel():
+    if request.method == "POST":
+        rezistente = request.form["input_rezistenta_paralel"]
+        rezistenta = rezistente.split(",")
+        m = 0
+        n = 1
+        for i in rezistenta:
+            rez = float(i)
+            n = n * rez
+            m = m + rez
+        p = round(n / m, 2)
+        r = f'{str(p)} Ω'
+        return render_template("/rezistenta_paralel.html", c=r)
+    else:
+        return render_template("/rezistenta_paralel.html")
 
 
 if __name__ == '__main__':
--- static/script.js
+++ static/script.js
@@ -10,3 +10,8 @@ function aprindere(){
 function stingere(){
     document.getElementById("cerc").style.background = 'transparent';
 }
+
+function numere_cu_virgula(input){
+    var numere = /[^0-9 . ,]/gi;
+    input.value = input.value.replace(numere,"");
+}
